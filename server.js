4484e6dab8cc88f1955fc35da160926f6a417542
Dan Warrick <danwarrickdev@gmail.com>
2024-05-10 21:14:26 -0400

    allow larger post

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to db"));
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 const BASE_URL = "/api";
